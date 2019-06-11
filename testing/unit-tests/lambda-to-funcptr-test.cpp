93649a947e7b014293a9fd541d447896a4704ef7
Nicholas Corgan <n.corgan@gmail.com>
2019-06-10 20:18:23 -0500

    Added skeleton unit test

---
+++ testing/unit-tests/lambda-to-funcptr-test.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2019 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "testlib.h"
+
+#include <gtest/gtest.h>
