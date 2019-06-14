fd7a8d47fe8d87d109fc5a98297871dd774544ae
Nicholas Corgan <n.corgan@gmail.com>
2019-06-13 20:20:34 -0500

    Brought in headers from https://github.com/sth/callable.hpp.git

---
+++ include/callable_to_funcptr/functor.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013, 2016 Stephan Hohe
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#if !defined(CALLABLE_FUNCTOR_HPP_INCLUDED)
+#define CALLABLE_FUNCTOR_HPP_INCLUDED
+
+#include "helpers.hpp"
+#include "function.hpp"
+#include "member_function.hpp"
+
+namespace detail {
+
+template<typename Class>
+using call_operator_traits = member_function_traits<decltype(&Class::operator())>;
+
+// classes with operator()
+template<typename Class>
+struct functor_traits : function_traits<typename call_operator_traits<Class>::function_type> {
+	typedef call_operator_traits<Class> call_operator; 
+};
+
+} // namespace detail
+
+
+template<typename Class>
+struct functor_traits : detail::functor_traits<detail::remove_cvref_t<Class>> {
+};
+
+#endif // CALLABLE_FUNCTOR_HPP_INCLUDED
+
